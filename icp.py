bb51298299367f76a9777d1cae41859f660eaacd
Arttu Mäkelä <arttumakela@outlook.com>
2021-02-10 06:08:04 +0200

    Added unit filtering

--- icp.py
+++ icp.py
@@ -1,5 +1,4 @@
 import openpyxl as xl
-import pandas as pd
 import os
 import glob
 from copy import copy
@@ -8,7 +7,7 @@ from win32com.client import Dispatch
 path = os.getcwd()
 xml_files = glob.glob(os.path.join(path, "*.xml"))
 excel_name = "Testing"
-exclusion_list = ["Blank", "Kalib.blank", "Std1", "Std2", "Std3", "QC", "qc", "Vesi", "vesi"]
+exclusion_list = ["Blank", "Kalib.blank", "Std1", "Std2", "Std3", "QC", "qc", "Vesi", "vesi"] # List of labels to filter out
 
 def convert_xls_to_xlsx(oldName:str, newName:str):
     oldName = os.path.abspath(oldName)
@@ -27,7 +26,8 @@ for file in xml_files:
     ws1 = wb.active
     wb.create_sheet("Sorted")
     ws2 = wb["Sorted"]
-    for row in ws1.iter_rows(min_row = 0, max_row = 2, min_col = 1, max_col = 13):
+
+    for row in ws1.iter_rows(min_row = 1, max_row = 2, min_col = 1, max_col = 13):
         for cell in row:
             ws2[f"{cell.coordinate}"] = cell.value
 
@@ -37,13 +37,30 @@ for file in xml_files:
             if cell.has_style:
                     ws2[f"{cell.coordinate}"]._style = copy(cell._style)
 
+    # Remove rows containing a cell containing a string from exclusion list
     for i in reversed(range(2, ws2.max_row+1)):
-        print(ws2.cell(row= i, column = 2).value)
         if ws2.cell(row= i, column = 2).value in exclusion_list:
             ws2.delete_rows(i)
-            print("deleted")
-
+    
+    # Strip g and ml units from a few columns
+    for row in ws2.iter_rows(min_row = 3, min_col= 5, max_col = 5):
+        for cell in row:
+            txt = cell.value
+            stripped = txt.strip(" g")
+            try:
+                cell.value = float(stripped)
+            except(ValueError):
+                continue
+    for row in ws2.iter_rows(min_row = 3, min_col= 6, max_col = 6):
+        for cell in row:
+            txt = cell.value
+            stripped = txt.strip(" ml")
+            try:
+                cell.value = float(stripped)
+            except(ValueError):
+                continue
     wb.save(excel_file)
+    wb.close()
 
 
 
