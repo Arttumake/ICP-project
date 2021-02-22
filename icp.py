3b6ea7f3f260b9a5c65efdc6c64014ffec03e1b5
Arttu Mäkelä <arttumakela@outlook.com>
2021-02-22 15:32:20 +0200

    major rework. Data is now sorted in separate tabs, ready to be listed in final sheet

--- icp.py
+++ icp.py
@@ -7,7 +7,11 @@ from win32com.client import Dispatch
 path = os.getcwd()
 xml_files = glob.glob(os.path.join(path, "*.xml"))
 excel_name = "Testing"
-exclusion_list = ["Blank", "Kalib.blank", "Std1", "Std2", "Std3", "QC", "qc", "Vesi", "vesi"] # List of labels to filter out
+exclusion_list = ("Blank", "Kalib.blank", "Std1", "Std2", "Std3", "QC", "qc", "Vesi", "vesi") # List of labels to filter out
+process_samples = ("AgPbR_", "ZnR_", "SR_", "M_", "J_", "ZnJ_", "AgPbJ_")
+courier_samples = (" AgPbR", " ZnR", " SR", " M", " J", " ZnJ", " AgPbJ")
+qc = ["Prep", "Pulp", "GBM", "Reag.blank", "GMR", "OREAS", "GSB", "Pyrref", "Pyr-ref", "SR-Ref"]
+not_geological = []
 
 def convert_xls_to_xlsx(oldName:str, newName:str):
     oldName = os.path.abspath(oldName)
@@ -59,8 +63,96 @@ for file in xml_files:
                 cell.value = float(stripped)
             except(ValueError):
                 continue
+
+    wb.create_sheet("Process")
+    wb.create_sheet("Courier")  
+    wb.create_sheet("Sorter")
+    wb.create_sheet("QC")  
+    wb.create_sheet("Final Sorted")
+
+    ws3 = wb["Process"]
+    ws4 = wb["Courier"]
+    ws5 = wb["Sorter"]
+    ws6 = wb["QC"]
+    
+    def move_items(from_sheet, to_sheet, tuple):
+        # Move data from a sheet to another if column contains string in list
+        sheet1_row = 2
+        sheet2_row = 0          
+        for row in from_sheet.iter_rows(min_row = 3, min_col = 2, max_col = 2): 
+            sheet1_row +=1
+            for cell in row:
+                if any(value in cell.value for value in tuple) and "Pulp".lower() not in cell.value.lower() and "Prep".lower() not in cell.value.lower():
+                    not_geological.append(cell.value)
+                    for row2 in from_sheet.iter_rows(min_row = sheet1_row, max_row = sheet1_row, min_col = 1):
+                        sheet2_row +=1
+                        for cell in row2:
+                            to_sheet.cell(row=sheet2_row, column = cell.col_idx).value = cell.value
+                            if cell.has_style:
+                                    to_sheet.cell(row=sheet2_row, column = cell.col_idx)._style = copy(cell._style)   
+                                   
+    def move_sorters(from_sheet, to_sheet):
+        sheet1_row = 2
+        sheet2_row = 0
+        for row in from_sheet.iter_rows(min_row = 3, min_col = 2, max_col = 2):
+            sheet1_row +=1
+            for cell in row:
+                if "_SO_" in cell.value:
+                    not_geological.append(cell.value)
+                    for row2 in from_sheet.iter_rows(min_row = sheet1_row, max_row = sheet1_row, min_col = 1):
+                        sheet2_row +=1
+                        for cell in row2:
+                            to_sheet.cell(row=sheet2_row, column = cell.col_idx).value = cell.value
+                            if cell.has_style:
+                                    to_sheet.cell(row=sheet2_row, column = cell.col_idx)._style = copy(cell._style)
+    def move_qc(from_sheet, to_sheet):
+        sheet1_row = 2
+        sheet2_row = 0
+        lower_qc = [value.lower() for value in qc]
+        for row in from_sheet.iter_rows(min_row = 3, min_col = 2, max_col = 2):
+            sheet1_row +=1
+            for cell in row:
+                """Checks if cell value has "prep" or "pulp" and if it does, loops through the column again and checks for the non-duplicate value
+                and inserts that value before the prep/pulp value to the QC tab
+                """
+                if any(value in cell.value.lower() for value in lower_qc):
+                    if "prep" in cell.value.lower() or "pulp" in cell.value.lower():
+                        print(f"duplicate {cell.value}")
+                        non_dup = cell.value.split("_", 1)
+                        if "pulp" in cell.value.lower():
+                            non_dup_split = non_dup[1].split(" pulp")
+                            non_dup_final = "_" + non_dup_split[0]
+                            print(non_dup_final)                           
+                        else:
+                            non_dup_split = non_dup[1].split(" prep")
+                            non_dup_final = "_" + non_dup_split[0]
+                            print(non_dup_final) 
+                        this_row = 2    
+                        for row in from_sheet.iter_rows(min_row = 3, min_col = 2, max_col = 2):
+                            this_row += 1
+                            for cell in row:
+                                if non_dup_final in cell.value and "prep" not in cell.value.lower() and "pulp" not in cell.value.lower():
+                                    print(cell.value)
+                                    for row in from_sheet.iter_rows(min_row = this_row, max_row = this_row, min_col = 1):
+                                        sheet2_row +=1
+                                        for cell in row:
+                                            to_sheet.cell(row=sheet2_row, column = cell.col_idx).value = cell.value
+                                            if cell.has_style:
+                                                    to_sheet.cell(row=sheet2_row, column = cell.col_idx)._style = copy(cell._style)                            
+                                    break
+                              
+                    not_geological.append(cell.value)
+                    for row2 in from_sheet.iter_rows(min_row = sheet1_row, max_row = sheet1_row, min_col = 1):
+                        sheet2_row +=1
+                        for cell in row2:
+                            to_sheet.cell(row=sheet2_row, column = cell.col_idx).value = cell.value
+                            if cell.has_style:
+                                    to_sheet.cell(row=sheet2_row, column = cell.col_idx)._style = copy(cell._style)                        
+
+    move_items(ws2,ws3, process_samples)
+    move_items(ws2,ws4, courier_samples)
+    move_sorters(ws2, ws5)
+    move_qc(ws2, ws6)
+   
     wb.save(excel_file)
     wb.close()
-
-
-
---
+++ run.bat
@@ -0,0 +1,3 @@
+C:\Users\arttu\AppData\Local\Programs\Python\Python38-32\python.exe C:\Users\arttu\Documents\icp-script\icp.py
+
+pause
